a0e3f9f04f62a7880d63152370bcba86b59fdf88
Carlos Camuzzi <carloscamuzzi.hs@gmail.com>
2025-02-08 16:16:44 -0300

    initial create

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
+  "use strict";
+
+  return Controller.extend("zordem.controller.View1", {
+    onInit: function () {},
+
+    onPress: function () {
+      alert("Hello Word!");
+    },
+  });
+});
---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["zordem/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
